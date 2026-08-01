4ed4d0feb1447257e918f96ce16f0c8a745f9597
ViT <vitali.cobelev@gmail.com>
2026-08-01 18:32:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Переключение экранов
-function showScreen(id) {
-  document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
-}
-
-// Главное меню → выбор режима
-function startGame() {
-  showScreen('mode');
-}
-
-
 
   // Переключение экранов
   function showScreen(id) {
